7be2983e8667d1883d09479ed6197f96aabd9eeb
lesenelir <miaomiaobabyzy@gmail.com>
2022-07-25 23:35:40 +0800

    update

--- 03-solidity-by-example/09-Gas/Gas.sol
+++ 03-solidity-by-example/09-Gas/Gas.sol
@@ -6,7 +6,8 @@ contract Gas {
 
     // 使用所有gas，直至交易失败
     function forever() public {
-        // 此处循环，花费所有gas spent，直至没有
+        // 修改state变量需要支付transaction交易手续费用
+        // 此处循环，花费所有gas spent（个数），直至没有
         // 随后交易失败
         while (true) {
             i += 1;
--- 03-solidity-by-example/11-for-and-while-loop/Loop.sol
+++ 03-solidity-by-example/11-for-and-while-loop/Loop.sol
@@ -16,7 +16,7 @@ contract Loop {
     }
 
     // while loop 和 do while 很少使用
-    // while 循环 和 do while 循环 显示上没有边界，可能
+    // while 循环 和 do while 循环 显示上没有边界，可能调用state变量，导致花费手续费，无限调用
     // uint j;
     // while (j < 10) {
     //     j++;
